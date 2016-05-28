52acbc2aaf93fd58d647aa2c80c1e7f0200e2b88
Sam Storie <storie@gmail.com>
2016-05-28 16:14:30 -0500

    Step 2 - Basic API with swagger enabled

---
+++ WeatherHistory/WeatherHistory.Web/Api/TemperaturesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WeatherHistory.Web.Api
+{
+    [RoutePrefix("temperatures")]
+    public class TemperaturesController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            var response = DateTime.Now;
+
+            return Ok(response);
+        }
+    }
+}
---
+++ WeatherHistory/WeatherHistory.Web/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Swashbuckle.Application;
+
+namespace WeatherHistory.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.Map("/api", api =>
+            {
+                var config = new HttpConfiguration();
+
+                config.MapHttpAttributeRoutes();
+
+                config.EnableSwagger(c =>
+                {
+                    c.RootUrl(req => SwaggerDocsConfig.DefaultRootUrlResolver(req) + "/api");
+                    c.SingleApiVersion("v1", "Weather History API");
+                })
+                .EnableSwaggerUi();
+
+                api.UseWebApi(config);
+            });
+        }
+    }
+}
